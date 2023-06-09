36fc020e702bc3f1410f6324979086edadf5d325
ChenYue <cheenyuee@foxmail.com>
2023-06-09 18:12:31 +0800

    Delete cmake-build-debug directory

--- cmake-build-debug/unitest/bitmap_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/Users/chenyue/Desktop/git/db_impl_course/cmake-build-debug/unitest/bitmap_test[1]_tests.cmake")
-  include("/Users/chenyue/Desktop/git/db_impl_course/cmake-build-debug/unitest/bitmap_test[1]_tests.cmake")
-else()
-  add_test(bitmap_test_NOT_BUILT bitmap_test_NOT_BUILT)
-endif()
